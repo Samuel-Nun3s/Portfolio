05cd9133bda5f0044d08bf5207b19fc6e4a8696d
Samuel Nunes <samuelsalomao883@gmail.com>
2025-07-05 22:17:15 -0300

    Header e Home finalizados

---
+++ script.js
@@ -0,0 +1,6 @@
+const buttonMenu = document.querySelector("#burguer");
+const nav = document.querySelector("nav");
+
+buttonMenu.addEventListener("click", () => {
+  nav.classList.toggle("hidden");
+})
