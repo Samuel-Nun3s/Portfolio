1c88360d31e95341bc244e0a4e1a274268bd07c8
Samuel Nunes <samuelsalomao883@gmail.com>
2025-07-06 14:41:16 -0300

    Responsividade da home finalizada

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ const nav = document.querySelector("nav");
 buttonMenu.addEventListener("click", () => {
   nav.classList.toggle("hidden");
 });
+
+window.addEventListener("load", () => {
+  const body = document.querySelector("body");
+  const widthBody = body.scrollWidth;
+
+  if (widthBody >= 600) {
+    nav.classList.remove("hidden");
+  }
+});
